3d934d8e3725ecad56cb1a665e2b2bc92d675451
Albert <alb.02@mail.ru>
2019-05-10 00:10:11 +0500

    Added login/register screen change

--- app/src/main/java/com/example/devicemanagement/AuthorisationActivity.java
+++ app/src/main/java/com/example/devicemanagement/AuthorisationActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AuthorisationActivity extends AppCompatActivity {
 
@@ -14,6 +16,8 @@ public class AuthorisationActivity extends AppCompatActivity {
     private SharedPreferences mSettings;
 
     private boolean loginScreen;
+    private Button switchLogin;
+    private Button switchRegister;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,47 @@ public class AuthorisationActivity extends AppCompatActivity {
 
         if (mSettings.contains(APP_PREFERENCES_INITED)) {
             loginScreen = true;
-            setContentView(R.layout.login_screen);
         } else {
+            SharedPreferences.Editor editor = mSettings.edit();
+            editor.putBoolean(APP_PREFERENCES_INITED, true);
+            editor.apply();
+
+
             loginScreen = false;
-            setContentView(R.layout.register_screen);
         }
+
+        changeScreen();
 
 
     }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+
+    View.OnClickListener changeScreenListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            changeScreen();
+        }
+    };
+
+    private void changeScreen() {
+        if (loginScreen) {
+            setContentView(R.layout.register_screen);
+
+            switchRegister = findViewById(R.id.s_register__change_screen);
+            switchRegister.setOnClickListener(changeScreenListener);
+        } else {
+            setContentView(R.layout.login_screen);
+
+            switchLogin = findViewById(R.id.s_login__change_screen);
+            switchLogin.setOnClickListener(changeScreenListener);
+        }
+        loginScreen = !loginScreen;
+    }
+
+
 }
